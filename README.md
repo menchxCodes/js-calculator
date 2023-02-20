99ba9ae7830e94fce03b24088dc0f9463f6e7fd4
menchx <43462510+menchxCodes@users.noreply.github.com>
2023-02-20 15:42:31 +0330

    Add Basic Logic

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # odin-calculator
 My attempt at creating a calculator for The Odin Project's calculator assigntment
+[Live Demo](https://menchxcodes.github.io/odin-calculator/)
---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+function divide(a,b) {
+    if(b!=0){
+        return a/b
+    } else {
+        return "ERROR"
+    }
+}
